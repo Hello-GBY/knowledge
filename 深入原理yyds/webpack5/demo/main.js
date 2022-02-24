c3d333a1595f1f16588e3b00103c0fb4b530601b
GuoBingYu <nameGBY@163.com>
2022-02-24 10:08:49 +0800

    fix(文件夹结构)

---
+++ 深入原理yyds/webpack5/demo/main.js
@@ -0,0 +1,8 @@
+import { add } from './common'
+
+function start () {
+    let num = add(1,2)
+    console.log('num: ', num);
+}
+
+start()
--- 深入原理yyds/webpack5/index.js
+++ 深入原理yyds/webpack5/index.js
@@ -3,3 +3,10 @@
 // 2. output 指定输出路径的文件夹中、 定义输出文件名
 
 // 3. module 模块， 一个模块对应一个文件
+
+// 竞品webpack vite rollup.js parcel
+
+
+// 插件
+// 扩展打包功能
+// 输出html文件
