198e3bf06cd83ba52c37546591743623954c407b
GuoBingYu <nameGBY@163.com>
2022-03-14 10:09:09 +0800

    feat(双向绑定)：数据劫持

--- 考点/vue/demo/vue.js
+++ 考点/vue/demo/vue.js
@@ -1,31 +1,80 @@
 class Vue {
   constructor(options) {
-    this.$data = options.data;
+    this.$data = options.data();
+    console.log('this.$data: ', this.$data);
+   
+    // 数据劫持
+    Observe(this.$data)
+  
+    // 属性代理 将vue上定义的属性之间设置到data上
+    Object.keys(this.$data).forEach(key => {
+      Object.defineProperty(this, key, {
+        enumerable: true,
+        configurable: true,
+        set(newValue){
+          this.$data[key] = newValue
+        },
+        get(){
+          return this.$data[key]
+        }
+      })
+    })
   }
-  // 数据劫持
-  Observe(data) {}
 }
 
 function Observe(obj) {
+  
+  if(!obj || typeof obj !== 'object') return
+
   Object.keys(obj).forEach((key) => {
     let value = obj[key];
+    // 递归掉用（深层调用）
+    Observe(value)
 
+    // 数据劫持
     Object.defineProperty(obj, key, {
       configurable: true,
       enumerable: true,
       set(newValue) {
-        console.log("newValue: ", newValue);
+       // 递归掉用 (当重新赋值时新对象的时候)
+        Observe(newValue);
+        console.log("obj: "+ key + '  被赋值了', newValue);
         value = newValue;
       },
       get() {
+        console.log("obj: "+ key + '  被获取值', value);
         return value;
       },
     });
   });
 }
-let obj = {
-  name: 123,
-};
-Observe(obj);
+// 订阅者
+class Dep {
+  constructor () {
+    this.sup =[]
+  }
 
-obj.name = "qw";
+  addSub(event) {
+    this.sup.push(event)
+  }
+  // 通告
+  notify() {
+    this.sup.forEach(event => {
+      event()
+    })
+  }
+}
+
+// 发布者
+
+class Watcher{
+  constructor (cb) {
+    this.cb = cb
+  }
+
+  // 修改dom
+  upaDate() {
+    this.cb()
+  }
+}
+
