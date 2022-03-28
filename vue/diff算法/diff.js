f58cbfd212301d4d2a833429caf18243ce9b19c8
gby <nameGBY@163.com>
2022-03-29 07:48:06 +0800

    feat(diff算法)：diff算法使用

---
+++ vue/diff算法/diff.js
@@ -0,0 +1,13 @@
+// 使用
+/**
+ * 有这个函数库 snabbdom
+ *
+ * git 上面有库 是ts的源码
+ *
+ * 直接 npm i -S snabbdom 可以进行安装
+ *
+ * 安装 webpack 5
+ * npm i -D webpack@5 webpack-cli@3 webpack-dev-server@3
+ *
+ *
+ */
