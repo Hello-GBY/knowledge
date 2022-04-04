7719b3be81b75e344bb52f5941d5bbdcdecdb548
gby <nameGBY@163.com>
2022-04-04 20:56:50 +0800

    feat(diff): 使用h函数

--- vue/diff算法/src/index.js
+++ vue/diff算法/src/index.js
@@ -18,7 +18,12 @@ const patch = init([
 
 // 创建虚拟节点
 const modelVNode = h("a", { props: { href: "https: //" } }, "xixixi");
+const modelVNode2 = h("ul", { class: "ulClass" }, [
+  h("li", "你好"),
+  h("li", "你好"),
+]);
 
 var container = document.getElementById("container");
-patch(container, modelVNode);
+// patch(container, modelVNode);
+patch(container, modelVNode2);
 console.log("modelVNode: ", modelVNode);
