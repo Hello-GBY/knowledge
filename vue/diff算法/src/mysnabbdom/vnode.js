2e6478e28a89081c8e87f6aa9f5b66c8327dadda
gby <nameGBY@163.com>
2022-04-05 13:44:19 +0800

    feat(diff): pacth函数特点

--- vue/diff算法/src/mysnabbdom/vnode.js
+++ vue/diff算法/src/mysnabbdom/vnode.js
@@ -1,5 +1,7 @@
 // vnode(sel, data, children, text, undefined);
 function VNode(sel, data, children, text, elm) {
-  return { sel, data, children, text, elm };
+  const key = data === undefined ? undefined : data.key;
+
+  return { sel, data, children, text, elm, key };
 }
 export default VNode;
