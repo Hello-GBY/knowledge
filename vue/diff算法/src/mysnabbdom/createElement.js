e717fee763a7895578d014130734ab7a7fd2b1e6
gby <nameGBY@163.com>
2022-04-09 15:34:37 +0800

    feat(diff): 修改createElement函数

--- vue/diff算法/src/mysnabbdom/createElement.js
+++ vue/diff算法/src/mysnabbdom/createElement.js
@@ -1,15 +1,20 @@
 // 真正创建节点
-export default function(VNode, pivot) {
+export default function(VNode) {
   // 把虚拟节点插入到基础树上
   let domNode = document.createElement(VNode.sel);
   console.log("domNode: ", domNode);
   // 判断是否含有子节点
   if (VNode.children != undefined && VNode.children.length) {
-    // VNode.children.insertBefore
+    // 内部是数组 子节点就需要递归了
+    for (let index = 0; index < VNode.children.length; index++) {
+      const h_ = VNode.children[index]; // 新的h函数
+      arguments.callee(h_);
+    }
   } else if (VNode.text != "") {
     domNode.innerText = VNode.text;
-    console.log("pivot: ", pivot);
-    console.log("pivot.parent: ", pivot.parentNode);
-    pivot.parentNode.insertBefore(domNode, pivot);
+    // pivot.parentNode.insertBefore(domNode, pivot);
   }
+  VNode.elm = domNode;
+
+  return VNode.elm;
 }
