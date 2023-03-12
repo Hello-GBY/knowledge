1c9236997ee6329010a26c201a323c968c38351f
gby <nameGBY@163.com>
2023-03-12 23:40:58 +0800

    feat(手写): 节流

--- 手写JS/手写.js
+++ 手写JS/手写.js
@@ -638,3 +638,23 @@ window.addEventListener(
 );
 
 
+// 节流
+// 设置一个标志
+function throttle(fn, delay) {
+  let flag = true;
+  return () => {
+    if (!flag) return;
+    flag = false;
+    timer = setTimeout(() => {
+      fn();
+      flag = true;
+    }, delay);
+  };
+}
+
+window.addEventListener(
+  "scroll",
+  throttle(() => {
+    console.log(111);
+  }, 1000)
+);
