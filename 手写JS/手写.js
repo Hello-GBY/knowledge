4f7668b41b0c6859e7c6da9811658ee6a68235ec
gby <nameGBY@163.com>
2023-03-22 21:58:16 +0800

    feat: 树形结构转成列表

--- 手写JS/手写.js
+++ 手写JS/手写.js
@@ -1212,3 +1212,59 @@ function listToTree(data) {
 
   return res
 }
+
+
+// 35 树形结构转成列表
+[
+  {
+      id: 1,
+      text: '节点1',
+      parentId: 0,
+      children: [
+          {
+              id:2,
+              text: '节点1_1',
+              parentId:1
+          }
+          {
+            id:3,
+            text: '节点1_1',
+            parentId:1
+        }
+      ]
+  }
+]
+转成
+[
+  {
+      id: 1,
+      text: '节点1',
+      parentId: 0 //这里用0表示为顶级节点
+  },
+  {
+      id: 2,
+      text: '节点1_1',
+      parentId: 1 //通过这个字段来确定子父级
+  }
+  ...
+]
+function treeToList(data){
+  let temp = []
+  let obj = data[0];
+
+  setInfo(obj);
+
+  function setInfo(item){
+    temp.push({
+      id: item.id,
+      text: item.text,
+      parentId: item.parentId,
+    })
+    
+    if(item.children) {
+      item.children.forEach(item => {
+        setInfo(item)
+      })
+    }
+  }
+}
