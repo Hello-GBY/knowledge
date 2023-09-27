4a0d2e32e7192b4c90b06c1cf611f914d03dac92
Hello-GBY <namegby@163.com>
2023-09-27 12:05:21 +0800

    feature

--- 2-深入原理/vue3源码/思考.js
+++ 2-深入原理/vue3源码/思考.js
@@ -12,7 +12,8 @@ https://cn.vuejs.org/guide/extras/render-function.html
 
 import { createApp } from 'vue'
 import App from './app'
-const app = createApp(App)
+const app = createApp(App) 
+// {mount (rootCantiner) => {// 有渲染方法} , _component: rootComponent // APP)}
 app.mount('#app')
 
 // 如何实现 createApp
@@ -21,9 +22,23 @@ function createApp (...args) {
   const app = ensureRenderer().createApp(...args) //  {render, createApp}
   const { mount } = app
   // 重写 mount 方法
-  // 目的： 标准的跨平台渲染流程是先创建 vnode，再渲染 vnode。此外参数 rootContainer 也可以是不同类型的值，比如，在 Web 平台它是一个 DOM 对象，而在其他平台（比如 Weex 和小程序）中可以是其他类型的值。所以这里面的代码不应该包含任何特定平台相关的逻辑，也就是说这些代码的执行逻辑都是与平台无关的。因此我们需要在外部重写这个方法，来完善 Web 平台下的渲染逻辑。
+  // 目的： 标准的跨平台渲染流程是先创建 vnode，再渲染 vnode。
+  //      此外参数 rootContainer 也可以是不同类型的值，
+  //      比如，在 Web 平台它是一个 DOM 对象，而在其他平台（比如 Weex 和小程序）中可以是其他类型的值。所以这里面的代码不应该包含任何特定平台相关的逻辑，也就是说这些代码的执行逻辑都是与平台无关的。因此我们需要在外部重写这个方法，来完善 Web 平台下的渲染逻辑。
   app.mount = (containerOrSelector) => {
-    // 
+    // 标准化容器（获取容器）
+    const container = normalizeContainer(containerOrSelector)
+    if (!container) return
+      
+    const component = app._component
+    // 如组件对象没有定义 render 函数和 template 模板，则取容器的 innerHTML 作为组件模板内容
+    if (!isFunction(component) && !component.render && !component.template) {
+      component.template = container.innerHTML
+    }
+    // 挂载前清空容器内容
+    container.innerHTML = ''
+    // 真正的挂载
+    return mount(container)
   }
   return app
 }
@@ -74,3 +89,136 @@ function createAppAPI (render) {
     return app
   }
 }
+// 以上就是 createApp 的实现，它的核心逻辑就是创建 app 对象，然后返回 app 对象。
+
+// ** const app = createApp(App)
+1. 返回了 一个对象 它的结构如下：
+2. 将APP组件挂载到 _component 上
+{
+  _component: rootComponent,
+  _props: rootProps,
+  _container: null,
+}
+
+// ** app.mount('#app')
+1. 重写mount方法  重写的目的是为了标准化容器 简单来说就是获取DOM对象
+2. 然后调用mount方法，传递的是DOM对象
+3. 其中核心
+    // todo: 为什么要创建vnode? 为什么要传递vnode和DOM对象?
+    vnode = createVNode(rootComponent, rootProps) // 传递的是APP组件
+    render(vnode, rootContainer) // 将vnode和DOM对象
+    // todo: vnode.component.proxy 是啥？
+4. 返回的是  vnode.component.proxy
+
+// ** 下面的核心是 createVNode, render
+// vnode = createVNode(rootComponent, rootProps)
+function createVNode(type, props = null ,children = null) {
+  if (props) {
+    // 处理 props 相关逻辑，标准化 class 和 style
+  }
+
+  // 对 vnode 类型信息编码
+  const shapeFlag = isString(type)
+    ? 1 /* ELEMENT */
+    : isSuspense(type)
+      ? 128 /* SUSPENSE */
+      : isTeleport(type)
+        ? 64 /* TELEPORT */
+        : isObject(type)
+          ? 4 /* STATEFUL_COMPONENT */
+          : isFunction(type)
+            ? 2 /* FUNCTIONAL_COMPONENT */
+            : 0
+
+  
+  const vnode = {
+    type,
+    props,
+    shapeFlag,
+    // 一些其他属性
+  }
+
+  // 标准化子节点，把不同数据类型的 children 转成数组或者文本类型
+  normalizeChildren(vnode, children)
+
+  return vnode
+}
+// vnode
+{ type : xx, props: xx, shapeFlag: xx, children: xx }
+// render(vnode, rootContainer)
+function render  (vnode, container) =>  {
+  if (vnode == null) {
+    // 销毁组件
+    if (container._vnode) {
+      unmount(container._vnode, null, null, true)
+    }
+  } else {
+    // 创建或者更新组件
+    patch(container._vnode || null, vnode, container)
+  }
+
+  // 缓存 vnode 节点，表示已经渲染
+  container._vnode = vnode
+}
+
+function patch (n1, n2, container, anchor = null, parentComponent = null, parentSuspense = null, isSVG = false, optimized = false) => {
+  // 如果存在新旧节点, 且新旧节点类型不同，则销毁旧节点
+  if (n1 && !isSameVNodeType(n1, n2)) {
+    anchor = getNextHostNode(n1)
+    unmount(n1, parentComponent, parentSuspense, true)
+    n1 = null
+  }
+
+  const { type, shapeFlag } = n2
+
+  switch (type) {
+    case Text:
+      // 处理文本节点
+      break
+    case Comment:
+      // 处理注释节点
+      break
+    case Static:
+      // 处理静态节点
+      break
+    case Fragment:
+      // 处理 Fragment 元素
+      break
+    default:
+      if (shapeFlag & 1 /* ELEMENT */) {
+        // 处理普通 DOM 元素
+        processElement(n1, n2, container, anchor, parentComponent, parentSuspense, isSVG, optimized)
+      }
+      else if (shapeFlag & 6 /* COMPONENT */) {
+        // 处理组件
+        processComponent(n1, n2, container, anchor, parentComponent, parentSuspense, isSVG, optimized)
+      }
+      else if (shapeFlag & 64 /* TELEPORT */) {
+        // 处理 TELEPORT
+      }
+      else if (shapeFlag & 128 /* SUSPENSE */) {
+        // 处理 SUSPENSE
+      }
+  }
+}
+
+const processComponent = (n1, n2, container, anchor, parentComponent, parentSuspense, isSVG, optimized) => {
+  if (n1 == null) {
+   // 挂载组件
+   mountComponent(n2, container, anchor, parentComponent, parentSuspense, isSVG, optimized)
+  }
+  else {
+    // 更新组件
+    updateComponent(n1, n2, parentComponent, optimized)
+  }
+}
+// 核心
+const mountComponent = (initialVNode, container, anchor, parentComponent, parentSuspense, isSVG, optimized) => {
+  // 创建组件实例
+  const instance = (initialVNode.component = createComponentInstance(initialVNode, parentComponent, parentSuspense))
+  // 设置组件实例
+  setupComponent(instance)
+  // 设置并运行带副作用的渲染函数
+  setupRenderEffect(instance, initialVNode, container, anchor, parentSuspense, isSVG, optimized)
+}
+
--- 6-算法/01-基础知识/A-复习- 基础知识.md
+++ 6-算法/01-基础知识/A-复习- 基础知识.md
@@ -85,16 +85,190 @@ dummy ： 当需要前驱节点的时候
 
 # 栈、队列
 
-例题：有效括号
-输入: "(]"
-输出: false
+1. 例题：有效括号
+   输入: "(]"
+   输出: false
 
+```js
 function queue(str) {
-let stack = [];
-let arr = str.split('');
+  let stack = [];
+  let arr = str.split("");
+  let map = {
+    "(": ")",
+    "[": "]",
+    "{": "}",
+  };
 
+  for (let i = 0; i < arr.length; i++) {
+    if (map[arr[i]]) {
+      stack.push(map[arr[i]]);
+    } else {
+      if (arr[i] !== stack.pop(arr[i])) return false;
+    }
+  }
+
+  return stack.length == 0;
 }
+```
 
-////
+2. 温度问题
 
-////
+# 队列
+
+1. 如何用栈实现一个队列？
+
+# DFS \ BFS
+
+深度优先遍历
+广度优先遍历
+
+怎么进行广度优先遍历？
+
+# 递归、回溯
+
+## 全排列？
+
+题目描述：给定一个没有重复数字的序列，返回其所有可能的全排列。
+
+```js
+// 全排列
+const permute = function (nums) {
+  const len = nums.length;
+  const cur = [];
+  const res = [];
+  const v = {};
+
+  function dfs(nth) {
+    if (nth === len) {
+      res.push(cur.slice());
+      return;
+    }
+
+    for (let i = 0; i < len; i++) {
+      if (!v[nums[i]]) {
+        cur.push(nums[i]);
+        v[nums[i]] = 1;
+        dfs(nth + 1);
+        v[nums[i]] = 0;
+        cur.pop();
+      }
+    }
+  }
+
+  dfs(0);
+
+  return res;
+};
+```
+
+## 组合问题：
+
+```js
+题目描述：给定一组不含重复元素的整数数组 nums，返回该数组所有可能的子集（幂集）。
+说明：解集不能包含重复的子集。
+
+示例: 输入: nums = [1,2,3]
+输出:
+[
+[3],
+[1],
+[2],
+[1,2,3],
+[1,3],
+[2,3],
+[1,2],
+[]
+]
+```
+
+```js
+const subsets = function (nums) {
+  // 初始化结果数组
+  const res = [];
+  // 缓存数组长度
+  const len = nums.length;
+  // 初始化组合数组
+  const subset = [];
+  // 进入 dfs
+  dfs(0);
+
+  // 定义 dfs 函数，入参是 nums 中的数字索引
+  function dfs(index) {
+    // 每次进入，都意味着组合内容更新了一次，故直接推入结果数组
+    res.push(subset.slice());
+    // 从当前数字的索引开始，遍历 nums
+    for (let i = index; i < len; i++) {
+      // 这是当前数字存在于组合中的情况
+      subset.push(nums[i]);
+      // 基于当前数字存在于组合中的情况，进一步 dfs
+      dfs(i + 1);
+      // 这是当前数字不存在与组合中的情况
+      subset.pop();
+    }
+  }
+  // 返回结果数组
+  return res;
+};
+```
+
+## 限定组合问题：及时回溯，即为“剪枝”
+
+题目描述：给定两个整数 n 和 k，返回 1 ... n 中所有可能的 k 个数的组合。
+
+示例: 输入: n = 4, k = 2
+输出:
+[
+[2,4],
+[3,4],
+[2,3],
+[1,2],
+[1,3],
+[1,4],
+]
+
+# 二叉树
+
+> 题目描述：给定一个二叉树，返回它的前序（先序）遍历序列。
+> 示例: 输入: [1,null,2,3]
+
+1  
+ \  
+ 2  
+ /  
+3
+
+## 先序遍历
+
+```js
+const preorderTraversal = function (root) {
+  let queue = [];
+  let list = [];
+  queue.push(root);
+  while (queue.length) {
+    let l = queue.pop();
+    list.push(l.val);
+    l.right && queue.push(l.right);
+    l.left && queue.push(l.left);
+  }
+  return list;
+};
+```
+
+## 后续遍历
+
+```js
+const preorderTraversal = function (root) {
+  let queue = [];
+  let list = [];
+  queue.push(root);
+  while (queue.length) {
+    let l = queue.pop();
+    list.unshift(l.val);
+    l.right && queue.push(l.right);
+    l.left && queue.push(l.left);
+  }
+  return list;
+};
+```
+
+192.168.0.0/16,10.0.0.0/8,172.16.0.0/12,127.0.0.1,localhost,_.local,timestamp.apple.com,sequoia.apple.com,seed-sequoia.siri.apple.com,192.168.0.0/16,10.0.0.0/8,172.16.0.0/12,127.0.0.1,localhost,_.local,timestamp.apple.com,sequoia.apple.com,seed-sequoia.siri.apple.com,seed.test.mindflow.work,\*.mindflow.work,api.test.mindflow.work
